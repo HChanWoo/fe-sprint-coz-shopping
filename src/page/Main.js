db7c35b38d973f386adac2b6cb18ed3f548f2dd8
HChanWoo <cksdn1st@naver.com>
2023-05-17 09:26:40 +0900

    feat Main link to other pages

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import { MainContainer, Container, TitleP } from "../styles/MainStyles";
+import { Link } from "react-router-dom";
 
 export default function Main() {
   const [cards, setCards] = useState([]);
@@ -27,11 +28,15 @@ export default function Main() {
   return (
     <MainContainer>
       <Container>
-        <TitleP>상품 리스트</TitleP>
+        <Link to="/products/list">
+          <TitleP>상품 리스트</TitleP>
+        </Link>
         <Card cards={cards} />
       </Container>
       <Container>
-        <TitleP>북마크 리스트</TitleP>
+        <Link to="/bookmark">
+          <TitleP>북마크 리스트</TitleP>
+        </Link>
         <Card cards={bookmark.slice(0, 4)} />
       </Container>
     </MainContainer>
